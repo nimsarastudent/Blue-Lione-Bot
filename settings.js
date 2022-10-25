1dfcabc4aac952c672cb09856ddf9c6b22882edb
nimsarastudent <116658296+nimsarastudent@users.noreply.github.com>
2022-10-25 20:21:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,9 +61,9 @@ global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy
-global.owner = ['94714188730'] //ur number 
-global.ownertag = '94714188730' //ur tag number
-global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
+global.owner = ['94774563898'] //ur number 
+global.ownertag = '94774563898' //ur tag number
+global.botname = '𝚛𝚊𝚜𝚑𝚒𝚗𝚍𝚞 𝙽𝙸𝚖𝚊𝚜𝚊𝚛𝚊 ' //ur bot name
 global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
 global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot' //script link
